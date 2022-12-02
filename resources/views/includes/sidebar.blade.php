3def3105f1a23c493650318caeb3bb2fd81a7b87
David Mendoza <davidmendozaofficial@gmail.com>
2022-12-03 01:00:38 +0800

    fix logo
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="index3.html" class="brand-link">
-        <img src="{{ asset('images/pharmacy_logo.png') }}" alt="Pharmacy Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">QR Transit</span>
+    <a href="{{ route('dashboard') }}" class="brand-link" style="text-align: center;">
+        <img src="{{ asset('qrtransit/img/qr-transit-logo.png') }}" alt="QR Transit Logo" class="brand-image" style="opacity: .8;">
+        {{-- <span class="brand-text font-weight-light">QR Transit</span> --}}
     </a>
 
     <div class="sidebar">
