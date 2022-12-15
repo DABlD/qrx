de6febdbf2e59060d9f1e3b1517d37484b1ba694
David Mendoza <davidmendozaofficial@gmail.com>
2022-12-15 21:53:02 +0800

    dashboard filter by company if company role
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,10 +12,32 @@ class DashboardController extends Controller
             return redirect()->route('sale.manifest');
         }
 
-        $users = User::whereIn('role', ['Admin', 'Coast Guard'])->count();
-        $routes = Route::count();
-        $vehicles = Vehicle::count();
-        $devices = Device::count();
+        $id = auth()->user()->id;
+
+        // USERS
+        $users = User::whereIn('role', ['Admin', 'Coast Guard', "Company"]);
+        $users = $users->count();
+
+        // ROUTES
+        $routes = Route::select('*');
+        if(auth()->user()->role == "Company"){
+            $routes = $routes->where('company_id', $id);
+        }
+        $routes = $routes->count();
+
+        // VEHICLES
+        $vehicles = Vehicle::select('*');
+        if(auth()->user()->role == "Company"){
+            $vehicles = $vehicles->where('company_id', $id);
+        }
+        $vehicles = $vehicles->count();
+
+        // DEVICES
+        $devices = Device::select('*');
+        if(auth()->user()->role == "Company"){
+            $devices = $devices->where('company_id', $id);
+        }
+        $devices = $devices->count();
 
         return $this->_view('dashboard', [
             'title'         => 'Dashboard',
