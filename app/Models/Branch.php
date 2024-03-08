833a0add2af6fdc82c29480761e4f94e51e52ef5
David Mendoza <davidmendozaofficial@gmail.com>
2024-03-08 23:11:19 +0800

    fix relationship 2
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -20,7 +20,7 @@ class Branch extends Model
     ];
 
     public function user(){
-        return $this->belongsTo('App\Models\User');
+        return $this->belongsTo('App\Models\User')->withTrashed();;
     }
 
     public function loans(){
