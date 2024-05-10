3cc337d1991a45daca46a6792f7deacaf003429d
David Mendoza <davidmendozaofficial@gmail.com>
2024-05-10 18:47:11 +0800

    final
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/loans.blade.php
+++ resources/views/exports/loans.blade.php
@@ -20,13 +20,27 @@
 			<td>{{ $data->branch->user->fname }}</td>
 			<td>{{ $data->contract_no }}</td>
 			<td>{{ $data->type }}</td>
-
 			<td>{{ "₱" . number_format($data->amount, 2, '.') }}</td>
 			<td>{{ $data->percent . "%" }}</td>
 			<td>{{ $data->paid_months . ' / ' . $data->months }}</td>
-			<td>{{ $data->amount }}</td>
-			<td>{{ $data->amount }}</td>
-			<td>{{ $data->amount }}</td>
+
+			{{-- MONTHLY PAYMENT --}}
+			@php
+				$percent = $data->percent / 100 / 12;
+				$amount = $data->amount;
+
+				$percent = 0.069;
+
+				$temp1 = ($percent) * $amount * pow((1 + ($percent)), $data->months);
+				$temp2 = (1 - pow((1 + ($percent)), $data->months));
+
+				$mp = $temp1 ?? 1 / $temp2 ?? 1;
+				$tp = $mp * $data->months;
+				$rev = $tp + $amount;
+			@endphp
+			<td>₱{{ number_format($mp, 2, '.') }}</td>
+			<td>₱{{ number_format($tp, 2, '.') }}</td>
+			<td>₱{{ number_format($rev, 2, '.') }}</td>
 			<td>{{ $data->status }}</td>
 			<td>{{ now()->parse($data->created_at)->format('M d, Y H:i:s') }}</td>
 		</tr>
